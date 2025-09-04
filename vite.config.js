ea711cc7adcb6e49136fbed2580dd5c8369badce
Adam <mundane.laps.6k@icloud.com>
2025-09-04 22:15:22 +0200

    HTML minification

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
 import { dirname, resolve } from "node:path";
+import htmlMinifier from "vite-plugin-html-minifier-terser";
 
 // ESM-friendly __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    htmlMinifier({
+      minifyOptions: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
