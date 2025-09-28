0983480c82b1ad548c0645bcdbfea011d15c9e77
Adam <mundane.laps.6k@icloud.com>
2025-09-28 16:25:35 +0200

    Rename exporters

--- src/hooks/useFileBase.js
+++ src/hooks/useFileBase.js
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { slug } from "@/lib/export/exporters";
+import { slug } from "@/lib/export/scales";
 
 export function useFileBase({ root, scale, accidental, strings }) {
   return useMemo(
